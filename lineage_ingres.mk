551388cfa97b7c4186f24dca92576c131455262e
dopaemon <polarisdp@gmail.com>
2025-11-07 13:05:34 +0000

    ingres: BringUp AxionAOSP
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- lineage_ingres.mk
+++ lineage_ingres.mk
@@ -10,6 +10,22 @@ $(call inherit-product, device/xiaomi/ingres/device.mk)
 # Inherit from common lineage configuration
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# AxionOS
+TARGET_DISABLE_EPPE := true
+TARGET_ENABLE_BLUR := true
+AXION_CAMERA_REAR_INFO := 64,8,2
+AXION_CAMERA_FRONT_INFO := 20
+AXION_MAINTAINER := KernelPanix
+AXION_PROCESSOR := Snapdragon_8_Gen_1
+BYPASS_CHARGE_SUPPORTED ?= false
+HBM_SUPPORTED := false
+TARGET_INCLUDE_VIPERFX := true
+TORCH_STR_SUPPORTED := true
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.perf.scroll_opt = true \
+    persist.sys.perf.scroll_opt.heavy_app = 2
+
 PRODUCT_NAME := lineage_ingres
 PRODUCT_DEVICE := ingres
 PRODUCT_MANUFACTURER := Xiaomi
